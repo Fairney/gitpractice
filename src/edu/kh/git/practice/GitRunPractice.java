ba4a221c6081733c52f14f78383776b7ee71ce4e
kks4116@naver.com <kks4116@gmail.com>
2023-02-20 19:20:11 +0900

    커밋

--- src/edu/kh/git/practice/GitRunPractice.java
+++ src/edu/kh/git/practice/GitRunPractice.java
@@ -5,7 +5,7 @@ public class GitRunPractice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("연습해보기");
-		System.out.println("두번째연습해보기");
+		System.out.println("두번째내용 수정해보기");
 	}
 
 }
